6ff1325834d55e7be1ff4f091ea6cc91685a8404
adwsssss <3408945207@qq.com>
2023-07-15 15:17:27 +0800

    改进ui

--- chess/view/mymainwindow.cpp
+++ chess/view/mymainwindow.cpp
@@ -6,7 +6,7 @@ MyMainWindow::MyMainWindow(QWidget *parent) :
     ui(new Ui::MyMainWindow)
 {
     ui->setupUi(this);
-
+    this->setWindowTitle("象棋游戏");
     connect(ui->common_button, &QPushButton::clicked, [=]{
         chess_board.show();
     });
